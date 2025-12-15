3b866ee1038e4093d6c277e2082751f8d4239a53
A-lex-Ra <rastorguev2047@gmail.com>
2025-12-15 22:57:49 +0300

    Configure for Railway deployment

--- server.js
+++ server.js
@@ -56,6 +56,16 @@ app.post("/api/items/modify", async (req, res) => {
 
 // Start server
 const PORT = process.env.PORT || 3000;
+const path = require("path");
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, "frontend/dist")));
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend/dist/index.html"));
+});
 
 broker.start().then(() => {
     app.listen(PORT, () => {
